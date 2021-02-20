6f12b9d3c74bbb35f9c6f56f94bb80edd99ac91c
Schneider Miner <schneiderminer@Schneiders-Air.hsd1.ut.comcast.net>
2021-02-20 12:57:34 -0700

    updated the making of the readme file.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# readmegenerator
+# Project 3
+## Description
+
+Athlete look up page and charities
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary depedencies, run the following command:
+
+```
+npm i
+```
+
+## Usage
+
+github.kminer-uofu
+
+## Contributing
+
+make sure you update often
+
+## Tests
+
+To run tests, run the following command:
+
+```
+npm test
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at sminer2006@comcast.net. You can find more of my work at [kminer-uofu](https://github.com/kminer-uofu/).
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,62 @@
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
+
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary depedencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
 
 `;
 }
